44a1ac7f244ec865a8554f99796b0c8b78288f76
ayushme53 <ayushme53@gmail.com>
2020-01-31 15:15:52 +0530

    connect

---
+++ CSE-C/Crud/input.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+    <title>Input Form</title>
+    </head>
+    
+    <body>
+    <form action="addRecord.php" method="post">
+      Name: <input type="text" name="username" placeholder="Enter name"><br>
+      Age:<input type="number" name="age" placeholder="Enter Age"><br>
+      Contact:<input type="text" name="contact" placeholder="EnterContact"><br>
+      Email: <input type="text" name="email" placeholder="Enter Email"><br>
+      <input type="submit" name='submit'>
+    </form>
+    </body>
+</html>
